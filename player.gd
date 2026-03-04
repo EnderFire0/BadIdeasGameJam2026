a0f7fb414bfa9bfb5ee97fd3a0d8b404b3f4d944
EnderFire0 <oliverjszczepanski@gmail.com>
2026-03-03 23:32:58 -0500

    Add crouching

--- player.gd
+++ player.gd
@@ -5,38 +5,48 @@ extends Area2D
 var playerVel : Vector2;
 var onGround : bool;
 var isJumping : bool;
+var isCrouching : bool;
+var facing : String;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	playerVel = Vector2.ZERO;
 	onGround = true;
 	isJumping = false;
-	
+	isCrouching = false;
+	facing = "right";
+	$AnimatedSprite2D.animation = "moveRight";
 	pass # Replace with function body.
 
+func get_velocity_sign() -> int:
+	if (playerVel.x < 0):
+		return -1;
+	elif (playerVel.x > 0):
+		return 1;
+	else:
+		return 0;
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func movement(delta: float) -> void:
 	# movement in the x-direction
 	var playerXAccel : float;
-	var speedDecaySign : float;
+	var speedDecaySign : float = 0;
 	if (Input.is_action_pressed("move_left") && Input.is_action_pressed("move_right")):
 		playerXAccel = -playerBaseAccel * sign(playerVel.x) * delta;
 	elif (Input.is_action_pressed("move_left")):
 		playerXAccel = -playerBaseAccel * delta;
-		$AnimatedSprite2D.animation = "moveLeft";
+		facing = "left";
 	elif (Input.is_action_pressed("move_right")):
 		playerXAccel = playerBaseAccel * delta;
-		$AnimatedSprite2D.animation = "moveRight";
+		facing = "right";
 	else:
-		if (playerVel.x < 0):
-			speedDecaySign = 1;
-		elif (playerVel.x > 0):
-			speedDecaySign = -1;
-		else:
-			speedDecaySign = 0;
+		speedDecaySign = -get_velocity_sign();
 		playerXAccel = 1.5 * playerBaseAccel * speedDecaySign * delta;
-	playerVel.x = clampf(playerVel.x + (playerXAccel), -playerMaxSpeed, playerMaxSpeed);
+	#for when player starts to crouch
+	if (abs(playerVel.x) > playerMaxSpeed):
+		playerVel.x = max(playerMaxSpeed, (abs(playerVel.x) - 0.75 * playerBaseAccel * delta)) * get_velocity_sign();
+	else:
+		playerVel.x = clampf(playerVel.x + (playerXAccel), -playerMaxSpeed, playerMaxSpeed);
+	#for cases where speed is small enough to not decay to 0(it fixes smth, idk)
 	if (sign(playerVel.x) == speedDecaySign):
 		playerVel.x = 0;
 	
@@ -55,3 +65,26 @@ func _process(delta: float) -> void:
 		playerVel.y = min(playerVel.y + playerBaseAccel * delta, playerBaseAccel * 4);
 	
 	position += playerVel * delta;
+
+func update_animation() -> void:
+	if (facing == "left"):
+		if (isCrouching):
+			$AnimatedSprite2D.animation = "crouchLeft";
+		else:
+			$AnimatedSprite2D.animation = "moveLeft";
+	else:
+		if (isCrouching):
+			$AnimatedSprite2D.animation = "crouchRight";
+		else:
+			$AnimatedSprite2D.animation = "moveRight";
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (Input.is_action_just_pressed("crouch") && onGround):
+		isCrouching = true;
+		playerMaxSpeed /= 2;
+	if (Input.is_action_just_released("crouch") && isCrouching):
+		isCrouching = false;
+		playerMaxSpeed *= 2;
+	movement(delta);
+	update_animation();
