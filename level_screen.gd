fe84ad16072fb6320f6d817b08a2ac32fcf1ca25
EnderFire0 <oliverjszczepanski@gmail.com>
2026-03-17 17:10:12 -0400

    Add camera to level_screen

--- level_screen.gd
+++ level_screen.gd
@@ -1,10 +1,14 @@
 extends Node
 
+var screenSize : Vector2;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$TextureRect.size = get_viewport().get_visible_rect().size;
-
-
+	screenSize = get_viewport().get_visible_rect().size;
+	$Camera2D.enabled = true;
+	$Camera2D.position = $Player.position;
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	moveCamera(delta);
+
+func moveCamera(delta: float) -> void:
+	$Camera2D.position = $Camera2D.position.lerp($Player.position, 4 * delta)
